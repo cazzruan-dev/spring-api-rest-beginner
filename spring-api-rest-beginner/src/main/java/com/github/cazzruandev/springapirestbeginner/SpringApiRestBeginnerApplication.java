973f1e9593b454ef51f7db9063b9e103d2d9115c
Carlos Ruan P Gomes <dev.cazzruan@gmail.com>
2021-02-02 22:31:44 -0300

    criação do projeto

---
+++ spring-api-rest-beginner/src/main/java/com/github/cazzruandev/springapirestbeginner/SpringApiRestBeginnerApplication.java
@@ -0,0 +1,13 @@
+package com.github.cazzruandev.springapirestbeginner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiRestBeginnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiRestBeginnerApplication.class, args);
+	}
+
+}
---
+++ spring-api-rest-beginner/src/test/java/com/github/cazzruandev/springapirestbeginner/SpringApiRestBeginnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.cazzruandev.springapirestbeginner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiRestBeginnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
